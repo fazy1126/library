b2bdc59277fd4cdbc515740491548589e1fb62a1
fazy <fazy.compro@gmail.com>
2021-10-21 01:21:55 +0900

    fix

--- test/aoj-grl-1-a.test.cpp
+++ test/aoj-grl-1-a.test.cpp
@@ -7,10 +7,10 @@ void Main(){
     int V,E,r;
     cin>>V>>E>>r;
     graph<ll> g(V);
-    REP(i,V){
+    REP(i,E){
         int s,t,d;
-        cin>>s>>t>>d,s--,t--;
-        g[s].eb(t);
+        cin>>s>>t>>d;
+        g[s].eb(t,d);
     }
     auto d=dijkstra(g,r);
     REP(i,V){
