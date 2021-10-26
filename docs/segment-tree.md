2ac178b2715410e5b698d748b4f351a858a89852
fazy1126 <fazy.compro@gmail.com>
2021-10-26 21:45:26 +0900

    update segment-tree

---
+++ docs/segment-tree.md
@@ -0,0 +1,14 @@
+---
+title: Segment Tree
+documentation_of: //structure/segment-tree.hpp
+---
+
+## 概要
+セグ木。
+
+## 使い方
+* `SegmentTree(n,f,e)`: サイズ`n`で初期化する。`f`は要素同士をマージする演算。`e`はモノイドの単位元。$O(n)$
+* `set(k,x)`: `k`番目の要素を`x`にする(更新はされない)。$O(1)$
+* `build()`: 木を構築する。初期化が終わったら呼ぶ必要がある。$O(n)$
+* `update(k,x)`: `k`番目の要素を`x`にする(一点更新)。$O(\log n)$
+* `query(a,b)`: 半開区間$[a,b)$に演算`f`を適用した結果を返す。$O(\log n)$
--- structure/segment-tree.hpp
+++ structure/segment-tree.hpp
@@ -5,12 +5,12 @@ struct SegmentTree{
     int sz;
     vector<T> data;
 
-    const F op;
+    const F f;
     const T e;
     
     SegmentTree()=default;
 
-    explicit SegmentTree(int n,const F& op,const T& e):op(op),e(e){
+    explicit SegmentTree(int n,const F& f,const T& e):f(f),e(e){
         sz=1;
         while(sz<n) sz<<=1;
         data.assign(2*sz,e);
@@ -21,22 +21,22 @@ struct SegmentTree{
     }
 
     void build(){
-        for(int k=sz-1;k>0;--k) data[k]=op(data[2*k],data[2*k+1]);
+        for(int k=sz-1;k>0;--k) data[k]=f(data[2*k],data[2*k+1]);
     }
 
     void update(int k,const T& x){
         k+=sz;
         data[k]=x;
-        while(k>>=1) data[k]=op(data[2*k],data[2*k+1]);
+        while(k>>=1) data[k]=f(data[2*k],data[2*k+1]);
     }
 
-    T query(int l,int r){
+    T query(int a,int b){
         T L=e,R=e;
-        for(l+=sz,r+=sz;l<r;l>>=1,r>>=1){
-            if(l&1) L=op(L,data[l++]);
-            if(r&1) R=op(R,data[--r]);
+        for(a+=sz,b+=sz;a<b;a>>=1,b>>=1){
+            if(a&1) L=f(L,data[a++]);
+            if(b&1) R=f(R,data[--b]);
         }
-        return op(L,R);
+        return f(L,R);
     }
 
     T operator[](const int& k) const{
